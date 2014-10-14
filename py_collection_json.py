a0fa949c2700bde3d003d5df652755d90e4eeb3c
Ian Evans <ianev@ubookstore.com>
2014-10-14 11:58:00 -0700

    + added license text * moved CollectionPlusJSONItem to be inside CollectionPlusJSON * changed super() calls to be 2 and 3 compatible * changed CollectionPlusJSON.append_item to accept args and create CollectionPlusJSONItem instance, rather than expect it. * changed CollectionPlusJSON.__init__ to accept extending args and apply them to collection instance property. * changed CollectionPlusJSON.paginate to accept endpoint argument. + added some docstrings

--- py_collection_json.py
+++ py_collection_json.py
@@ -8,47 +8,50 @@ from math import ceil
 COLLECTION_JSON = 'application/vnd.collection+json'
 
 
-class CollectionPlusJSONItem(dict):
-
-    def __contains__(self, item):
-        if item in self.data:
-            return True
-        else:
-            return False
-
-    def __delitem__(self, key):
-        del self.data[key]
-
-    def __getitem__(self, item):
-        return self.data[item]
-
-    def __init__(self, uri=None, **kwargs):
-        if uri is not None:
-            self.href = uri
-        self.data = kwargs
-        super(CollectionPlusJSONItem, self).__init__(self.__dict__)
-
-    def __setitem__(self, key, value):
-        self.data[key] = value
-
-    def __str__(self):
-        return json.dumps(self)
-
-    __repr__ = __str__
-
-    def get(self, k, d=None):
-        if k in self.data:
-            return self.data[k]
-        else:
-            return d
-
-
 class CollectionPlusJSON(dict):
 
     # TODO: Make this subclass a collection type with some fancy extras?
 
     mimetype = COLLECTION_JSON
-    __CollectionPlusJSONItem = CollectionPlusJSONItem
+
+    class CollectionPlusJSONItem(object):
+
+        def __contains__(self, item):
+            if item in self.data:
+                return True
+            else:
+                return False
+
+        def __delitem__(self, key):
+            del self.data[key]
+
+        def __getitem__(self, item):
+            return self.data[item]
+
+        def __init__(self, uri=None, **kwargs):
+            if uri is not None:
+                self.href = uri
+            self.data = kwargs
+            try:
+                # Python 3
+                super().__init__(self.__dict__)
+            except TypeError:
+                # Python 2
+                super(CollectionPlusJSON.CollectionPlusJSONItem, self).__init__(self.__dict__)
+
+        def __setitem__(self, key, value):
+            self.data[key] = value
+
+        def __str__(self):
+            return json.dumps(self)
+
+        __repr__ = __str__
+
+        def get(self, k, d=None):
+            if k in self.data:
+                return self.data[k]
+            else:
+                return d
 
     def __contains__(self, item):
         if item in self.collection:
@@ -73,8 +76,7 @@ class CollectionPlusJSON(dict):
                  ],
                  template={
                      'data': []
-                 }):
-        # TODO: accept params to specify in collection?
+                 }, **kwargs):
         self.collection = {
             'version': str(version),
             'href': href,
@@ -84,7 +86,13 @@ class CollectionPlusJSON(dict):
             'template': template,
             'error': error
         }
-        super(CollectionPlusJSON, self).__init__(collection=self.collection)
+        self.collection = dict(self.collection, **kwargs)  # zip together standard collection + extended properties
+        try:
+            # Python 3
+            super().__init__(collection=self.collection)
+        except TypeError:
+            # Python 2
+            super(CollectionPlusJSON, self).__init__(collection=self.collection)
 
     def __setitem__(self, key, value):
         self.collection[key] = value
@@ -94,12 +102,24 @@ class CollectionPlusJSON(dict):
 
     __repr__ = __str__
 
-    def append_item(self, item):
-        if not (isinstance(item, CollectionPlusJSON.__CollectionPlusJSONItem)):
-            raise TypeError('item must be a CollectionPlusJSONItem instance!')
+    def append_item(self, uri, data):
+        """
+        Append an item to this collection's 'items' property.
+        :param uri: A string representing this resource's URI.
+        :param data: This resource representation's data, as a dict or
+        :return:
+        """
+        item = CollectionPlusJSON.CollectionPlusJSONItem(uri=uri, **data)
         self.collection.get('items').append(item)
 
     def append_link(self, uri, rel, prompt):
+        """
+        Append a link to this collection's 'links' property.
+        :param uri: A string representing the link's URI.
+        :param rel: The link's relationship to this resource.
+        :param prompt: The prompt to optionally display for this link.
+        :return:
+        """
         self.collection.get('links').append({'href': uri, 'rel': rel, 'prompt': prompt})
 
     def get(self, k, d=None):
@@ -108,8 +128,21 @@ class CollectionPlusJSON(dict):
         else:
             return d
 
-    def paginate(self, uri_template='{endpoint_uri}?page={page}&per_page={per_page}', page=1, per_page=5, leading=2,
-                 trailing=2):
+    def paginate(self, endpoint='', uri_template='{endpoint_uri}?page={page}&per_page={per_page}', page=1, per_page=5,
+                 leading=2, trailing=2):
+        """
+        Paginate this collection, automatically trimming items and adding appropriate links for navigation.
+        :param uri_template: A string providing a template for paginated URI structure. May include the following keys:
+        "{endpoint_uri}" - This will evaluate to the value of the 'endpoint' param.
+        "{page}" - The page number will be inserted here.
+        "{per_page}" - The number of items to display per page will be inserted here.
+        :param endpoint: The URI for this resource.
+        :param page: The desired page for this representation.
+        :param per_page: The number of items per page for this representation.
+        :param leading: The number of leading pages before "this" page to add to this collection's "links".
+        :param trailing: The number of trailing pages after "this" page to add to this collection's "links".
+        :return:
+        """
         if (type(page) is not int) or (type(per_page) is not int):
 
             try:
