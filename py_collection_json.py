5feef55033c1509a5575d04ee5211c39e3c66cf9
Ian Evans <ianev@ubookstore.com>
2014-10-15 16:50:58 -0700

    * small fixes to please PyCharm's linter

--- py_collection_json.py
+++ py_collection_json.py
@@ -31,6 +31,11 @@ class CollectionPlusJSON(UserDict):
                 self.code = code
             if message is not None:
                 self.message = message
+            try:
+                # Python 3
+                super().__init__()
+            except ValueError:
+                super(CollectionPlusJSON.Error, self).__init__()
 
     class Item(UserDict, BaseCollectionItem):
 
@@ -72,6 +77,12 @@ class CollectionPlusJSON(UserDict):
                 self.render = render
             if prompt is not None:
                 self.prompt = prompt
+            try:
+                # Python 3
+                super().__init__()
+            except TypeError:
+                # Python 2
+                super(CollectionPlusJSON.Link, self).__init__()
 
     class Query(Item):
 
