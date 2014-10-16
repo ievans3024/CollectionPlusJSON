c512d4dddeefd9b01624c972e7432ffed9385318
Ian Evans <ianev@ubookstore.com>
2014-10-16 15:14:16 -0700

    + added a test

--- tests.py
+++ tests.py
@@ -12,4 +12,33 @@ def test_correct_standard_properties():
     else:
         return True
 
+
+def test_incorrect_standard_properties():
+    to_return = False
+    try:
+        CollectionPlusJSON(links=None, items=[], queries=[], template=CollectionPlusJSON.Template(),
+                           error=CollectionPlusJSON.Error())
+    except TypeError:
+        to_return = True
+    try:
+        CollectionPlusJSON(links=[], items=None, queries=[], template=CollectionPlusJSON.Template(),
+                           error=CollectionPlusJSON.Error())
+    except TypeError:
+        to_return = True
+    try:
+        CollectionPlusJSON(links=[], items=[], queries=None, template=CollectionPlusJSON.Template(),
+                           error=CollectionPlusJSON.Error())
+    except TypeError:
+        to_return = True
+    try:
+        CollectionPlusJSON(links=[], items=[], queries=[], template=None, error=CollectionPlusJSON.Error())
+    except TypeError:
+        to_return = True
+    try:
+        CollectionPlusJSON(links=[], items=[], queries=[], template=CollectionPlusJSON.Template(), error=None)
+    except TypeError:
+        to_return = True
+    return to_return
+
 assert test_correct_standard_properties()
+assert test_incorrect_standard_properties()
