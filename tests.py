392f0011d2dc755e63144cc5782ffdae950ca98a
Ian Evans <ianev@ubookstore.com>
2014-10-20 15:13:11 -0700

    + added some tests

--- tests.py
+++ tests.py
@@ -7,7 +7,8 @@ def incorrect_standard_properties():
     """
     Test that a CollectionPlusJSON object will not init successfully with any of the standard optional arguments
     set to a number of basic incorrect types.
-    :return: True if not successfully instantiated, False if a TypeError is thrown
+    :raises AssertionError: If arg values that should be considered invalid do not raise a TypeError
+    :return:
     """
     args = ('links', 'items', 'queries', 'template')
     test_values = (None, '', {}, (), 1)
@@ -34,9 +35,26 @@ def incorrect_standard_properties():
 # Default init with no args, should not raise errors
 CollectionPlusJSON()
 
-# init with acceptable arg types, should not raise errors
+# Correct types in arguments should not raise errors
 CollectionPlusJSON(links=[], items=[], queries=[], template=CollectionPlusJSON.Template(),
                    error=CollectionPlusJSON.Error())
 
-# incorrect types should throw errors
+# Incorrect types in arguments should raise errors
 incorrect_standard_properties()
+
+# Instance should accept mutable-iterable-style item getting, setting and deleting
+collection = CollectionPlusJSON()
+collection['test'] = 'test'
+collection['test']
+del collection['test']
+
+# Inner classes should be appear to be the same in an instance or direct from class.
+assert CollectionPlusJSON.BaseCollectionItem == collection.BaseCollectionItem
+assert CollectionPlusJSON.Error == collection.Error
+assert CollectionPlusJSON.Item == collection.Item
+assert CollectionPlusJSON.Link == collection.Link
+assert CollectionPlusJSON.Query == collection.Query
+assert CollectionPlusJSON.Template == collection.Template
+
+# Instance str and repr should appear to be the same
+assert str(collection) == repr(collection)
