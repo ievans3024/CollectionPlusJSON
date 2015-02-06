c091d0c29ce7c83292b1f7e6c42076b4a2f0240c
Ian Evans <ianev@ubookstore.com>
2015-02-06 12:55:59 -0800

    + Travis CI setup + python package setup + added test suite

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+import collection_plus_json
+from setuptools import setup
+
+setup(
+    name="CollectionPlusJSON",
+    version=collection_plus_json.__version__,
+    url="https://github.com/ievans3024/CollectionPlusJSON",
+    license="MIT",
+    author=collection_plus_json.__author__,
+    description="Some python bindings for the Collection+JSON Hypermedia Type"
+    # test_suite="tests.test_all"
+)
--- tests.py
+++ tests.py
@@ -1,5 +1,8 @@
 __author__ = 'Ian S. Evans'
 
 from collection_plus_json import Array, Collection, Data, Error, Item, Link, Query, Template
+from unittest import TestCase, TestSuite
 
-# TODO: unittest!
+# TODO: write tests
+
+test_all = TestSuite()
