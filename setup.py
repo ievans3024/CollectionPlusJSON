6f7b119b751e92a3fb17ccc6ee837d02593336f0
Ian Evans <ianev@ubookstore.com>
2015-02-06 13:02:11 -0800

    * fixed some requirements for travis builds to pass

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     url="https://github.com/ievans3024/CollectionPlusJSON",
     license="MIT",
     author=collection_plus_json.__author__,
-    description="Some python bindings for the Collection+JSON Hypermedia Type"
-    # test_suite="tests.test_all"
+    description="Some python bindings for the Collection+JSON Hypermedia Type",
+    test_suite="tests.test_all"
 )
